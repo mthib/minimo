7f4a75d3d8114a635bcc1e82410e0f20cb912af2
Marc Thibault <marc.thibault73@gmail.com>
2019-01-04 15:16:23 +0100

    pb colonne grid

--- view/articlesView.php
+++ view/articlesView.php
@@ -1,25 +1,24 @@
-<div class="contenu">
 <?php 
 $compteur = 0;
 foreach($articlesFiles as $element)
 {
 	if(($compteur % 2) == 0) // ouvre une div au premier article
-		echo '<div class="grid-x grid-padding-x margeContenu">';
+		echo '<div class="grid-x grid-padding-x align-center margeContenu">
+				<div class="large-9 cell">';
 	
 	echo '<div class="large-6 medium-6 cell">';
-	echo '<img src="assets/images/'.$element[1]->post_name().'"/>';
-	echo '<h2>'.strtoupper($element[0]->post_category()).'</h2>';
+	echo '<img src="assets/images/'.$element[1]->post_name().'"/>'; // element[1] => objet file
+	echo '<h2>'.strtoupper($element[0]->post_category()).'</h2>';	// element[0] => objet article
 	echo '<h1>'.$element[0]->post_title().'</h1>';
 	echo '<p class="texte">'.substr($element[0]->post_content(),0,200).'...</p>'; // affiche les 200 premiers caracteres du contenu
 	echo '</div>';
 	
 	if(($compteur % 2) == 1)
-		echo '</div>'; // ferme la div au deuxieme article
+		echo '</div></div>'; // ferme la div au deuxieme article
 	
 	$compteur++;
 }
 ?>
-</div>
 	
 		
 			
--- view/firstArticleView.php
+++ view/firstArticleView.php
@@ -3,14 +3,14 @@
 		<img src="assets/images/<?= $fileFirstArticle->post_name(); ?>" class="imagePrincipale"/>
 	</div>
 </div>
-<div class="contenu">
-	 <div class="grid-x grid-padding-x margeContenu">
-		<div class="large-12 cell">
-			<h2><?= strtoupper($firstArticle->post_category()); ?></h2>
-			<h1><?= $firstArticle->post_title(); ?></h1>
-			<p class="texte"><?= $firstArticle->post_content(); ?>
-			</p>
-			<h2>LEAVE A COMMENT</h2>
-		</div>
+ <div class="grid-x grid-padding-x align-center margeContenu">
+	<div class="large-9 cell">
+		<h2><?= strtoupper($firstArticle->post_category()); ?></h2>
+		<h1><?= $firstArticle->post_title(); ?></h1>
+		<p class="texte"><?= $firstArticle->post_content(); ?>
+		</p>
+		<h2>LEAVE A COMMENT</h2>
 	</div>
 </div>
+
+
